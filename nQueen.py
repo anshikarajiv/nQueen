a93c3680c909e7fc5b0262b930e9a0e4606060e7
anshikarajiv <56602914+anshikarajiv@users.noreply.github.com>
2020-05-13 22:44:25 +0800

    Create nQueen.py

---
+++ nQueen.py
@@ -0,0 +1,19 @@
+# Put your testing code here
+# This function will be executed when you run your program on VPL
+# This function will not be graded
+def main():
+  print("nothing done.")
+
+#Task 1.1
+def countAttack(queen):
+  return 0
+
+#Task 1.2
+def getNext(queen):
+  bestQueen = list(queen)
+  return tuple(bestQueen)
+
+#Task 1.3
+def solve(queen, n):
+  solution = list(queen)
+  return tuple(solution)
